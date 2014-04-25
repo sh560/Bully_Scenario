1de8dafde17e36589f3f68ba7abeb95320325161
Paul Wisdom <paul@flybuy.com>
2014-04-24 18:01:39 -0700

    Start of master git branch

---
+++ Scenario_XML_Model/XML_Define.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+[XmlRoot("LevelCollection")]
+public class LevelContainer{
+
+	[XmlElement("Level")]
+	public Level[] Levels;
+
+	//public List<Level> Levels = new List<Level>();
+
+	public static LevelContainer Load(string path){
+		var serializer = new XmlSerializer (typeof(LevelContainer));
+		using (var stream = new FileStream(path, FileMode.Open))
+		{
+			return serializer.Deserialize (stream) as LevelContainer;
+		}
+
+	}	
+}
+public class Level{
+	[XmlAttribute("name")]
+	public string name;
+
+
+	[XmlElement("Quest")]
+	public Quest[] Quests;
+}
+
+public class Quest{
+	[XmlAttribute("name")]
+	public string name;
+
+	[XmlElement("Dialogue")]
+	public string dialogue;
+
+	[XmlElement("NumAnswers")]
+	public int numAnswers;
+
+	[XmlElement("Answer")]
+	public string[] Answers;
+}
